5c3d5a09fa79dfefe49bc377d6f4887f8ba67033
OM1GA <845085139@qq.com>
2023-10-29 13:22:20 +0800

    CI/CD 5.1

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,22 @@
-<scriptsetup>import{ref}from"vue";importaxiosfrom"axios";constlist=ref([]);constgetList=async()=>{try{constresponse=awaitaxios.get("http://127.0.0.1:8888/list");console.log(response.data);returnresponse.data;}catch(error){console.error(error);}};getList().then((data)=>{list.value=data;});</script><template><templatev-for="item in list":key="item.id"><h2>{{item}}</h2></template></template>
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+const list = ref([]);
+const getList = async () => {
+  try {
+    const response = await axios.get("http://127.0.0.1:8888/list");
+    console.log(response.data);
+    returnresponse.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+getList().then((data) => {
+  list.value = data;
+});
+</script>
+<template>
+  <template v-for="item in list" :key="item.id">
+    <h2>{{ item }}</h2></template
+  >
+</template>
